0642a3a4698edc55f7f9b0636806ce4e7ddf3bd7
chris <s86610000@gmail.com>
2015-12-22 20:00:38 +0800

    Step1.2 利用 AreEqual() 驗證相等。測試失敗。

--- MsTestIntroduction/1-AssertSample.cs
+++ MsTestIntroduction/1-AssertSample.cs
@@ -77,8 +77,8 @@ namespace MsTestIntroduction
 			};
 
 			//this test will pass; when you override Equals(), AreEqual will invoke Order's Equals(), rather than Object's Equals()
-			Assert.AreSame(expected, actual); //驗證是否為同一個物件（相同）, 等同於 Assert.IsTrue(Object.RefrenceEquals(expected,actual))
-			//Assert.AreEqual(expected, actual); //驗證兩個物件是否相等（相等）, 等同於 Assert.IsTrue(Object.Equals(a,b))
+			//Assert.AreSame(expected, actual); //驗證是否為同一個物件（相同）, 等同於 Assert.IsTrue(Object.RefrenceEquals(expected,actual))
+			Assert.AreEqual(expected, actual); //驗證兩個物件是否相等（相等）, 等同於 Assert.IsTrue(Object.Equals(a,b))
 		}
 
 		[TestMethod]
